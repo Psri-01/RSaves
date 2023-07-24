a761902d00ae0c2a120b9ddcc886652361abe87e
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-24 20:21:08 +0530

    Create dataframes.R

---
+++ dataframes.R
@@ -0,0 +1,20 @@
+install.packages("tidyverse")
+library(tidyverse)
+library(ggplot2)
+data("diamonds")
+View(diamonds)
+head(diamonds)
+str(diamonds)
+glimpse(diamonds)
+colnames(diamonds)
+#Cleaning data
+rename(diamonds, carat_new = carat)
+rename(diamonds, carat_new = carat, cut_new = cut)
+summarize(diamonds, mean_carat = mean(carat))
+library(tidyverse)
+mutate(diamonds,carat_2=carat*100)
+#Visualizing data
+ggplot(data = diamonds, aes(x = carat, y = price)) +  geom_point()
+ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) +  geom_point()
+ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) +  geom_point() + facet_wrap(~cut)
+#creates a different plot for each type of cut. `ggplot2` makes it easy to do this with the `facet_wrap()` function
