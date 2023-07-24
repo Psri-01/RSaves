4a8a4624ac0068c4cac7998d506c526665d2035c
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-25 00:15:50 +0530

    Create Organize.R

---
+++ Organize.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+penguins %>% arrange(bill_length_mm)
+#descending order
+penguins %>% arrange(-bill_length_mm)
+penguins2 <- penguins %>% arrange(-bill_length_mm)
+View(penguins2)
+penguins %>% group_by(island) %>% drop_na() %>% summarize(mean_bill_length_mm=mean(bill_length_mm))
+penguins %>% group_by(island) %>% drop_na() %>% summarize(max_bill_length_mm=max(bill_length_mm))
+penguins %>% group_by(species,island) %>% drop_na() %>% summarize(max_bl=max(bill_length_mm),mean_blm=mean(bill_length_mm))
+penguins %>% filter(species=="Adelie")
