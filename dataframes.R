0d2529db1658aeeadec4f9833620b7b953a9a5c4
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-24 20:27:40 +0530

    Update dataframes.R

--- dataframes.R
+++ dataframes.R
@@ -18,3 +18,18 @@ ggplot(data = diamonds, aes(x = carat, y = price)) +  geom_point()
 ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) +  geom_point()
 ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) +  geom_point() + facet_wrap(~cut)
 #creates a different plot for each type of cut. `ggplot2` makes it easy to do this with the `facet_wrap()` function
+
+library(tidyverse)
+names <- c('Peter','Jennifer','Julie','Alex')
+age <- c(15,19,20,25)
+people <- data.frame(names,age)
+head(people)
+str(people)
+glimpse(people)
+colnames(people)
+mutate(people, age_in_20=age+20)
+
+fruit <- c("Lemon", "Blueberry", "Grapefruit", "Mango", "Strawberry")
+rank <- c(4,2,5,3,1)
+fruit_ranks<-data.frame(fruit,rank)
+View(fruit_ranks)
