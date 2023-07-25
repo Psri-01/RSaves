b73abe2762e47c6370e2a084ab015a0dc0d7ccdf
Psri-01 <114862496+Psri-01@users.noreply.github.com>
2023-07-25 20:30:35 +0530

    Update Cleaning.R

--- Cleaning.R
+++ Cleaning.R
@@ -22,6 +22,17 @@ rename_with(penguins,tolower)
 library(janitor)
 clean_names(penguins)
 
+library(tidyverse)
+penguins %>% arrange(bill_length_mm)
+#descending order
+penguins %>% arrange(-bill_length_mm)
+penguins2 <- penguins %>% arrange(-bill_length_mm)
+View(penguins2)
+penguins %>% group_by(island) %>% drop_na() %>% summarize(mean_bill_length_mm=mean(bill_length_mm))
+penguins %>% group_by(island) %>% drop_na() %>% summarize(max_bill_length_mm=max(bill_length_mm))
+penguins %>% group_by(species,island) %>% drop_na() %>% summarize(max_bl=max(bill_length_mm),mean_blm=mean(bill_length_mm))
+penguins %>% filter(species=="Adelie")
+
 install.packages("tidyverse")
 install.packages("skimr")
 install.packages("janitor")
